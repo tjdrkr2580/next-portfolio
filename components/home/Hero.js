a33773c9afb4670ca90e797270f9352d75e1a2bf
Kim Tae Hyun <tjdrkr2580@naver.com>
2022-08-10 04:53:24 +0900

    설치 next-themes

--- components/home/Hero.js
+++ components/home/Hero.js
@@ -1,4 +1,6 @@
 import Animation from "./../Animation";
+import Link from "next/link";
+
 export default function Hero() {
   return (
     <>
@@ -17,9 +19,11 @@ export default function Hero() {
           역사를 것이다.
         </p>
         <div className="flex justify-center">
-          <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-            프로젝트 보러가기
-          </button>
+          <Link href="/projects">
+            <a className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+              프로젝트 보러가기
+            </a>
+          </Link>
         </div>
       </div>
       <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
